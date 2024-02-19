9ef96f11079cef5dded6f61d75c2047edbb4e011
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2024-02-19 15:37:02 +0200

    update structure

--- main.js
+++
@@ -1,5 +0,0 @@
-import "./style.scss"
-import "./src/js/checkEmail.js"
-import "./src/js/checkTel.js"
-import "./src/js/formSubmit.js"
-
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./style.scss"
+import "./js/checkEmail.js"
+import "./js/checkTel.js"
+import "./js/formSubmit.js"
+
